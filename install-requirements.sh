21b1d0cd186683c84a2ddb3f3f30bff2d732efdc
chenjun3092 <chenjun3092@163.com>
2018-03-28 13:21:06 +0800

    update install-requirements.sh and fix .vimrc

--- install-requirements.sh
+++ install-requirements.sh
@@ -2,7 +2,8 @@
 
 sudo apt -y install ctags
 sudo apt -y install python-setuptools
-sudo asy_install flake8 
+sudo easy_install flake8 
 sudo apt -y install nodejs
 sudo apt -y install npm 
 sudo npm install jshint -g
+sudo npm i -g eslint
